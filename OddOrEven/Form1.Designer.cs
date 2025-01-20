18d7654f7956f8a8492d4dcb59f20d0c5b1012e2
U-Guneysu <82709766+U-Guneysu@users.noreply.github.com>
2025-01-20 21:56:53 +0300

    v1

---
+++ OddOrEven/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace OddOrEven
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(215)))), ((int)(((byte)(234)))));
+            this.label1.Location = new System.Drawing.Point(107, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayınızı Giriniz : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.button1.Font = new System.Drawing.Font("Sylfaen", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(215)))), ((int)(((byte)(234)))));
+            this.button1.Location = new System.Drawing.Point(200, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sonuç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(296, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 17);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Unispace", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(153, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tek mi Çift mi?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(57)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tek mi Çift mi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OddOrEven/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace OddOrEven
+{
+    public partial class Form1 : Form
+    {
+        double girilensayi;
+        const string upper = "HATA!";
+        const string success = "Hesaplama Başarılı!";
+        const string info = "Bilgilendirme";
+     
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                if(textBox1.Text == "-")
+                {
+                        MessageBox.Show("Lütfen Sadece '-' Operatörü Koymayınız ve Tekrar Deneyiniz", upper,
+                                      MessageBoxButtons.OK,
+                                      MessageBoxIcon.Warning);
+                    
+                }
+                else
+                {
+                    girilensayi = Convert.ToDouble(textBox1.Text);
+                    if (girilensayi % 2 == 0)
+                    {
+                        MessageBox.Show("Girdiğiniz sayı : " + girilensayi + "\n" + "Sonuç :" + " Çift", success, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Girdiğiniz sayı : " + girilensayi + "\n" + "Sonuç :" + " Tek", success, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Bir Sayı Giriniz ve Tekrar Deneyiniz", upper, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if(textBox1.Text == "Lütfen Bir Değer Giriniz")
+            {
+                MessageBox.Show("Gireceğiniz değer yazı OLMAMALIDIR", upper, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            /*const yazarak 'upper' değişkenini string sabiti haline getirdim. Olası durumlarda çıkacak hataları önlemek amaçlı da MessageBox'a kullanıcı uyarması için bildiriler ekledim. */
+
+
+            if (textBox1.Text.Contains("--"))
+            {
+                MessageBox.Show("Lütfen Girdiğiniz Değerin Yanına Birden Fazla '-' veya '+' Koymadan Tekrar Deneyiniz", upper,
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                e.Cancel = false;
+            }
+            else if (textBox1.Text.Contains(girilensayi + "-"))
+            {
+                MessageBox.Show("Lütfen Girdiğiniz Değerin Sonuna Birden Fazla '-' veya '+' Koymadan Tekrar Deneyiniz", upper,
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Warning);
+            }
+        }
+            
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+       
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            const string message =
+                "Uygulamayı kapatmak istediğinize emin misiniz?";
+            const string caption = "Tek mi Çift mi Kapatılıyor ";
+            // 'result' değişkeni var ile tanımlandığında otomatik olarak string değişkeni olarak tanımlanır.
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question); 
+
+            // Eğer Kullanıcı "Hayır" Tuşuna Basarsa.   
+            if (result == DialogResult.No)
+            {
+                // Sistem Kendisini Kapatmasını Engeller ve Çalışmaya (Değer Beklemeye ve İşlemeye) Devam Eder.  
+                e.Cancel = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (   e.KeyChar >= 'a' && e.KeyChar <= 'z' || e.KeyChar >= 'A' && e.KeyChar <= 'Z' 
+                || e.KeyChar == '"' || e.KeyChar == '!' || e.KeyChar == ' ' || e.KeyChar == '#'
+                || e.KeyChar == '$' || e.KeyChar == '½' || e.KeyChar == '{' || e.KeyChar == '[' 
+                || e.KeyChar == ']' || e.KeyChar == '}' || e.KeyChar == '<' || e.KeyChar == '>'
+                || e.KeyChar == '#' || e.KeyChar == '|' || e.KeyChar == 'é' || e.KeyChar == '!'
+                || e.KeyChar == '^' || e.KeyChar == '%' || e.KeyChar == '&' || e.KeyChar == '.'
+                || e.KeyChar == '/' || e.KeyChar == '(' || e.KeyChar == ')' || e.KeyChar == '='
+                || e.KeyChar == '?' || e.KeyChar == '_' || e.KeyChar == 'ş' || e.KeyChar == ';'
+                || e.KeyChar == ':' || e.KeyChar == ',' || e.KeyChar == '£' || e.KeyChar == '\\'
+                || e.KeyChar == 'ç' || e.KeyChar == 'ö' || e.KeyChar == 'ı' || e.KeyChar == 'ğ'
+                || e.KeyChar == 'ü' || e.KeyChar == '+' || e.KeyChar == '*')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true; // Enter tuşunun varsayılan işlemini engeller
+                button1.Focus(); // Odaklanmak istediğiniz TextBox'a geçiş yapar
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if(textBox1.Text.IndexOf('-') >= 1)
+            {
+                for (int i = 1; i < textBox1.Text.Length;  i++)
+                {
+                    if (textBox1.Text[i] == '-')
+                    {
+                        MessageBox.Show("'-' Operatörü Sayının Başında Olmalıdır Lütfen Tekrar Deneyiniz.", upper, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox1.Text = textBox1.Text.Remove(i, 1);
+                        textBox1.SelectionStart = textBox1.Text.Length;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
